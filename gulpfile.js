6dcacc1620c33fd4330655dc47db292894a00546
Marcus Noble <marcus@marcusnoble.co.uk>
2017-12-18 18:47:17 +0000

    Added swipe controls for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var paths = {
     './src/web-slide-title.html',
     './src/web-presentation-keyboardcontrols.html',
     './src/web-presentation-progress.html',
-    './src/web-code-sample.html']
+    './src/web-code-sample.html',
+    './src/web-presentation-swipecontrols.html']
 };
 
 gulp.task('dist', function() {
